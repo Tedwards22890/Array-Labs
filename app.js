4b43b9c248f86144440fd5a538239dcbffba3b65
Tedwards <108353463+Tedwards22890@users.noreply.github.com>
2022-09-11 01:50:19 -0400

    Problem 2 solved

--- app.js
+++ app.js
@@ -141,7 +141,7 @@ console.log("Vegetarian Food: ", veggyFood);
 
 function italianResults() {
   let results = dishes.filter(function (el) {
-    if (el.cuisine === "Italian") {
+    if (el.cuisine === "Italian" && el.servings > 5) {
       return true;
     } 
   });
@@ -149,7 +149,7 @@ function italianResults() {
 }
 
 let italyFood = italianResults();
-console.log("Iltalian Food: ", italyFood);
+console.log("Iltalian Food Greater than 5: ", italyFood);
 
 //3. Create a function that will return only dishes whose serving id number matches their serving count.
 //Filter
