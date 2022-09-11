92472b2a8a2fa8bb5afcd865b939008d825f20b3
Tedwards <108353463+Tedwards22890@users.noreply.github.com>
2022-09-11 02:21:01 -0400

    Problem 7 solved

--- app.js
+++ app.js
@@ -215,6 +215,15 @@ console.log("Types: ", cuisineCount);
 //7. Create a function that will append the cuisine type to the start of the dish's name. Ie, "Italian Pizza"
 //Map
 
+let cuisineName = dishes.map(
+  function(el) 
+  {
+    return `${el.cuisine} ${el.name}`
+  }
+);
+
+console.log("New dish names: ", cuisineName);
+
 //8. Create a function that will append the cuisine type to the start of the dish's name. Then, return only the Vegetarian dish objects. So this function should return objects 11-13 with their names changed to "Vegetarian Lasagna", "Vegetarian Falafel", "Vegetarian Chili"
 //Map, Filter
 
